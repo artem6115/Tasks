bdcaf24ff2e19e6e8ff625299fff8bcbce510d64
Миков Артём <artem.mikov.2003@mail.ru>
2023-10-13 18:01:10 +0500

    Добавление слоя DataLayer
    -Таблица Задач
    -Таблица Деректорий
    -Создание связей и миграций

---
+++ Data/DataLayer/Entities/Tasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+	public class Tasks : Page
+	{
+		public int IdDirectory { get; set; }
+		public Directories Directory { get; set; }
+	}
+}
---
+++ Data/DataLayer/TaskDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebTasks.Data.DataLayer
+{
+	public class TaskDbContext :DbContext
+	{
+		public TaskDbContext(DbContextOptions<TaskDbContext>options):base(options){}
+		public DbSet<Directories> Directories { get; set; }
+		public DbSet<Tasks> Tasks { get; set; }
+
+	}
+	
+}
