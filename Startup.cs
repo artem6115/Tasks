994d94a55898566ae67c91526bd45f79f2352998
Миков Артём <artem.mikov.2003@mail.ru>
2023-10-13 18:02:25 +0500

    Создание бд и миграций
    -исправлена ошибка в файле настроек
    -в список сервисов добавлена бд
    -игноре бд и миграций в гит

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebTasks.Data.DataLayer;
 
 namespace WebTasks
 {
@@ -23,6 +25,8 @@ namespace WebTasks
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			string DbStr =Configuration.GetConnectionString("ConnectionString") ;
+			services.AddDbContext<TaskDbContext>(options=>options.UseSqlite(DbStr));
 			services.AddControllersWithViews();
 		}
 
