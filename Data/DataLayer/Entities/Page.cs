b12d36fcdfaa9f89f0b18220d39787f96245346f
Артём Миков <artem.mikov.2003@mail.ru>
2023-10-15 18:03:41 +0500

    Добовление автоинкремента

--- Data/DataLayer/Entities/Page.cs
+++ Data/DataLayer/Entities/Page.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace DataLayer.Entities
 {
 	public class Page
 	{
-		public int Id { get; set; }
+        [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 		public string Title { get; set; }
 		public string Html { get; set; }
 
